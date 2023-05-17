36bd03a78e3162f5552614776e8efd98a9d0d607
Mr Greg <gregsw123@gmail.com>
2023-05-16 23:42:18 -0600

    ultimo víd

--- resources/views/impresion/seleccion.blade.php
+++ resources/views/impresion/seleccion.blade.php
@@ -76,7 +76,6 @@
                             </div>
                         </div>
                     </div>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/1hwPl5qmMbo?start=98?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
