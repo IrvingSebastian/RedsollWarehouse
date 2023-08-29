631d16f13e67b2dda370882d85786e747189437a
unknown <ramirezerick677@gmail.com>
2023-08-29 17:08:00 -0600

    catemachx7

--- app/Exports/SalidaExport.php
+++ app/Exports/SalidaExport.php
@@ -14,6 +14,23 @@ class SalidaExport implements FromCollection, WithHeadings
     public function collection()
     {
         // Escribir aqui la lógica para obtener las salidas
+          // Obtener todas las devoluciones
+          $Salida = P_Salidas::with('pieza:id,codigo,descripcion')
+          ->select('id_pieza', 'cantidad', 'created_at')
+          ->get();
+
+      // Formatear la colección para que se muestren los datos que se desean
+      $formattedDevoluciones = $devoluciones->map(function ($devolucion) {
+          return [
+              'id_pieza' => $devolucion->id_pieza, // Acceder al atributo 'id_pieza
+              'codigo_pieza' => $devolucion->pieza->codigo, // Acceder al atributo 'codigo' de la relación 'pieza'
+              'descripcion' => $devolucion->pieza->descripcion, // Acceder al atributo 'descripcion' de la relación 'pieza'
+              'cantidad' => $devolucion->cantidad, // Acceder al atributo 'cantidad'
+              'created_at' => $devolucion->created_at, // Acceder al atributo 'created_at'         
+          ];
+      });
+
+      return $formattedDevoluciones;
     }
 
     public function headings(): array
